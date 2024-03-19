bad48aafc4cc581d147fad3ab6433a9b35fe074d
ElCarnival <pakun4ik@gmail.com>
2024-03-19 03:29:06 +0300

    asd

--- calc.go
+++ calc.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-
-	fmt.Println("this is IT")
+	fmt.Println("Введите значение")
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	text = strings.TrimSpace(text)
+	toNumber, _ := strconv.Atoi(text)
+	fmt.Println(toNumber)
 }
